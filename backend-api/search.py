25785d6685eb0d454087f4f0a832511921d37427
Ivan <24IMC11196@imc.ac.at>
2025-10-26 17:45:59 +0100

    Add search by Firmenbuchnummer

--- backend-api/search.py
+++ backend-api/search.py
@@ -1,5 +1,5 @@
 from client import create_client
-
+from datetime import date
 
 def search_by_name(company_name, exact_search = True):
     client = create_client() #for now
@@ -26,14 +26,37 @@ def search_by_name(company_name, exact_search = True):
     for ergebnis in ergebnisse:
         company_info = { #included this for english translation and in case we decide to remove some fields later
             "fnr": ergebnis.FNR,
-            "status": ergebnis.STATUS,
+            "status": "active" if "STATUS" in ergebnis else "inactive",
             "name": ergebnis.NAME,
             "location": ergebnis.SITZ,
             "legal_form": {"code": ergebnis.RECHTSFORM.CODE, "text": ergebnis.RECHTSFORM.TEXT},
-            "legal_status": ergebnis.RECHTSEIGENSCHAFT,
+            "legal_status": "active" if "RECHTSEIGENSCHAFT" in ergebnis else "inactive",
             "responsible_court": {"code": ergebnis.GERICHT.CODE, "text": ergebnis.GERICHT.TEXT}
         }
         results.append(company_info)
 
 
     return results
+
+def search_by_fnr(company_fnr):
+    client = create_client()
+
+    suche_params = {
+        "FNR": company_fnr,
+        "STICHTAG": date.today(),
+        "UMFANG": "Kurzinformation"
+    }
+
+    suche_response = client.service.AUSZUG_V2_(**suche_params)
+
+    firma = suche_response.FIRMA
+
+    legal_form_entry = firma.FI_DKZ07 if len(firma.FI_DKZ07) > 0 else None
+
+    return {
+        "fnr": company_fnr,
+        "name": firma.FI_DKZ02[0].BEZEICHNUNG,
+        "location": firma.FI_DKZ06[0].SITZ,
+        "legal_form": {"code": legal_form_entry.RECHTSFORM.CODE, "text": legal_form_entry.RECHTSFORM.TEXT} if legal_form_entry else None,
+        "legal_status": "active" if legal_form_entry and legal_form_entry.AUFRECHT else "inactive"
+    }
