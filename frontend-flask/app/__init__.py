3842c57f990338d92a6f450006d1e844488be17e
Ivan <24IMC11196@imc.ac.at>
2025-11-05 13:27:11 +0100

    Add configuration files

--- frontend-flask/app/__init__.py
+++ frontend-flask/app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_pyfile('../config.py')
+
     from .routes import main
     app.register_blueprint(main)
 
