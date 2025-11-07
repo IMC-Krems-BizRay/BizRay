87a9516703838a644061d8a59e0ae065ce9cb41a
magdalena-ivanova-ds <magdalena.ivanova.ds@gmail.com>
2025-11-07 16:28:35 +0100

    Update __init__.py

--- frontend-flask/app/__init__.py
+++ frontend-flask/app/__init__.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, session
+from .models import db, bcrypt
+from flask_migrate import Migrate
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    from . import config as app_config
+    app.config.from_object(app_config)
 
-    app.config.from_pyfile('../config.py')
+    db.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
 
+    @app.context_processor
+    def inject_logged_in():
+        return {"logged_in": session.get("logged_in")}
+
     return app
+
