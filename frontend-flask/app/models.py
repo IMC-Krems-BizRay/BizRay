56f35dc290f32f9e79ec41789f9e92c4def92a22
magdalena-ivanova-ds <magdalena.ivanova.ds@gmail.com>
2025-11-07 16:28:40 +0100

    Update models.py

---
+++ frontend-flask/app/models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
